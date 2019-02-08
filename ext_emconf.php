3eb9468c1f133c62bc42e1af7690a0927736dcb3
Sebastian Fischer <typo3@evoweb.de>
2019-02-08 07:23:37 +0100

    Commit state of extension in TER

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "imagemap_wizard".
+#
+# Auto generated 08-08-2015 06:01
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Imagemap Wizard',
+	'description' => 'Provides an TYPO3 Wizard which enables interactive Imagemap-Creation - related to the TYPO3-Linkwizard. Supported by AOE media.',
+	'category' => 'be',
+	'shy' => 0,
+	'version' => '0.6.2',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '3.8.0-4.6.99',
+	'PHP_version' => '5.0.0-5.3.99',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => 'tt_content',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Tolleiv Nietsch',
+	'author_email' => 'extensions@<myfirstname>.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
