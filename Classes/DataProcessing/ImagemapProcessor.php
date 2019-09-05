44cbcceaa2a7674656f9eaa6170f730adc0e4261
Sebastian Fischer <typo3@evoweb.de>
2019-09-05 22:11:54 +0200

    Move conditions of area attributes from template to data processor

--- Classes/DataProcessing/ImagemapProcessor.php
+++ Classes/DataProcessing/ImagemapProcessor.php
@@ -50,7 +50,25 @@ class ImagemapProcessor implements \TYPO3\CMS\Frontend\ContentObject\DataProcess
                     $attributes = array_diff($attributes, ['target']);
                 }
 
-                $processedData['imageMap'] = $mapArray;
+                $areas = [];
+                foreach ($mapArray as &$area) {
+                    $areaAttributes = $area;
+                    unset($areaAttributes['link']);
+
+                    foreach ($areaAttributes as $key => $value) {
+                        if (!in_array($key, $attributes)) {
+                            $areaAttributes['data-' . $key] = $value;
+                            unset($areaAttributes[$key]);
+                        }
+                    }
+
+                    $areas[] = [
+                        'link' => $area['link'],
+                        'attributes' => $areaAttributes,
+                    ];
+                }
+
+                $processedData['imageMap'] = $areas;
                 $processedData['imageMapAttributes'] = $attributes;
             }
             $processedData['imageMapName'] = $mapName;
