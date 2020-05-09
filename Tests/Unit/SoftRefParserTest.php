ecc8f015859e8a20a9a54d40c4e87dee7b025567
Sebastian Fischer <typo3@evoweb.de>
2020-05-09 12:16:29 +0200

    Refactor unit test

--- Tests/Unit/SoftRefParserTest.php
+++ Tests/Unit/SoftRefParserTest.php
@@ -1,7 +1,8 @@
 <?php
+
 namespace Evoweb\Imagemap\Tests\Unit;
 
-/**
+/*
  * This file is developed by evoWeb.
  *
  * It is free software; you can redistribute it and/or modify it under
@@ -12,113 +13,81 @@ namespace Evoweb\Imagemap\Tests\Unit;
  * LICENSE.txt file that was distributed with this source code.
  */
 
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Evoweb\Imagemap\Database\SoftRefParser;
+
 class SoftRefParserTest extends \TYPO3\TestingFramework\Core\Unit\UnitTestCase
 {
-    /**
-     * @var \Evoweb\Imagemap\Database\SoftRefParser|\PHPUnit\Framework\MockObject\MockObject
-     */
-    protected $processor;
+    protected $resetSingletonInstances = true;
 
-    protected function setUp(): void
-    {
-        $this->processor = $this->getMockBuilder(\Evoweb\Imagemap\Database\SoftRefParser::class)
-            ->setMethods(['getTypoLinkParts'])
-            ->getMock();
-    }
-
-    protected function tearDown(): void
-    {
-        unset($this->processor);
-    }
-
-    /**
-     * @test
-     */
-    public function emptySoftRefsWork()
-    {
-        $emptyStrings = ['', '[]'];
-
-        foreach ($emptyStrings as $str) {
-            $result = $this->processor->findRef('', '', '', $str, '', [], '');
-            $this->assertEquals(true, is_array($result), 'array expected');
-            $this->assertEquals(true, isset($result['content']), '"content"-part in array expected');
-            $this->assertEquals(
-                true,
-                isset($result['elements']) && is_array($result['elements']),
-                '"elements"-part in array as sub-array expected'
-            );
-        }
-    }
-
-    /**
-     * @test
-     */
-    public function basicSoftRefsWork()
-    {
-        $this->processor
-            ->expects($this->any())
-            ->method('getTypoLinkParts')
-            ->will($this->returnCallback([$this, 'getTypoLinkPartsCallback']));
-
-        $mapContent = '[{coords:"0,0,100,100",shape:"rect",link:"t3://page?uid=1"}]';
-        $result = $this->processor->findRef('', '', '', $mapContent, '', [], '');
-
-        $this->assertCount(1, $result['elements'], 'Wrong Reference-Count found');
-
-        $element = array_pop($result['elements']);
-        $this->assertEquals('t3://page?uid=1', $element['matchString'], 'Wrong Reference found');
-        $this->assertEquals('string', $element['subst']['type'], 'Wrong Reference-Type found');
-        $this->assertEquals('t3://page?uid=1', $element['subst']['tokenValue'], 'Wrong token value found');
-        $this->assertStringContainsString(
-            '{softref:' . $element['subst']['tokenID'] . '}',
-            $result['content'],
-            'Token not found in parsed content'
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function multipleSoftRefsWork()
-    {
-        $this->processor
-            ->expects($this->any())
-            ->method('getTypoLinkParts')
-            ->will($this->returnCallback([$this, 'getTypoLinkPartsCallback']));
-
-        $mapContent = '[{link:"t3://page?uid=1"},{link:"t3://page?uid=2"},{link:"t3://page?uid=3"}]';
-        $result = $this->processor->findRef('', '', '', $mapContent, '', [], '');
-
-        $this->assertCount(3, $result['elements'], 'Wrong Reference-Count found');
-
-        $supposed = [
-            't3://page?uid=1',
-            't3://page?uid=2',
-            't3://page?uid=3',
-        ];
-        $index = 0;
-        foreach ($result['elements'] as $token => $element) {
-            $this->assertEquals('string', $element['subst']['type'], 'Wrong Reference-Type found');
-            $this->assertEquals($supposed[$index], $element['matchString'], 'Wrong Reference found');
-            $this->assertEquals($supposed[$index], $element['subst']['tokenValue'], 'Wrong token value found');
-            $this->assertStringContainsString(
-                '{softref:' . $element['subst']['tokenID'] . '}',
-                $result['content'],
-                'Token (' . $index . ') not found in parsed content'
-            );
-            $index++;
-        }
-    }
-
-    public function getTypoLinkPartsCallback($typolinkValue)
+    public function findRefReturnsParsedElementsDataProvider(): array
     {
         return [
-            'target' => '',
-            'class' => '',
-            'title' => '',
-            'additionalParams' => '',
-            'LINK_TYPE' => 'url',
-            'url' => $typolinkValue,
+            'link to page' => [
+                [
+                    'map_areas' => [
+                        'content' => '[{coords:"0,0,100,100",shape:"rect",link:"t3://page?uid=42"}]',
+                        'elementKey' => 1,
+                        'matchString' => 't3://page?uid=42',
+                    ],
+                ],
+                [
+                    'subst' => [
+                        'type' => 'db',
+                        'recordRef' => 'pages:42',
+                        'tokenValue' => 42,
+                    ],
+                ],
+            ],
+            'links to page' => [
+                [
+                    'map_areas' => [
+                        'content' => '[{link:"t3://page?uid=40"},{link:"t3://page?uid=41"},{link:"t3://page?uid=42"}]',
+                        'elementKey' => 3,
+                        'matchString' => 't3://page?uid=42',
+                    ],
+                ],
+                [
+                    'subst' => [
+                        'type' => 'db',
+                        'recordRef' => 'pages:42',
+                        'tokenValue' => 42,
+                    ],
+                ],
+            ],
         ];
+    }
+
+    /**
+     * @test
+     * @dataProvider findRefReturnsParsedElementsDataProvider
+     * @param array $softrefConfiguration
+     * @param array $expectedElement
+     */
+    public function findRefReturnsParsedElements(array $softrefConfiguration, array $expectedElement)
+    {
+        $eventDispatcherProphecy = $this->prophesize(EventDispatcherInterface::class);
+        foreach ($softrefConfiguration as $softrefKey => $configuration) {
+            /** @var EventDispatcherInterface $eventDispatcher */
+            $eventDispatcher = $eventDispatcherProphecy->reveal();
+            $subject = new SoftRefParser($eventDispatcher);
+            $result = $subject->findRef(
+                'tt_content',
+                'bodytext',
+                1,
+                $configuration['content'],
+                $softrefKey,
+                []
+            );
+
+            self::assertArrayHasKey('elements', $result);
+            self::assertArrayHasKey($configuration['elementKey'], $result['elements']);
+
+            // Remove tokenID as this one depends on the softrefKey and doesn't need to be verified
+            unset($result['elements'][$configuration['elementKey']]['subst']['tokenID']);
+
+            $expectedElement['matchString'] = $configuration['matchString'];
+            self::assertEquals($expectedElement, $result['elements'][$configuration['elementKey']]);
+        }
     }
 }
